b693144629d1ee5b9944e04ba1f4d171946acbba
anyars-encarta <anyarsencarta@gmail.com>
2024-02-14 12:07:34 +0000

    Install linters

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import TreeViewNavMenu from './components/tree-view-nav-menu/index';
-import { menus } from './components/tree-view-nav-menu/data';
+import menus from './components/tree-view-nav-menu/data';
 import './components/tree-view-nav-menu/styles.css';
 
-const App = () => {
-  return (
-    <div className="menu-list-container">
-      <TreeViewNavMenu menus={menus} />
-    </div>
-  );
-}
+const App = () => (
+  <div className="menu-list-container">
+    <TreeViewNavMenu menus={menus} />
+  </div>
+);
 
 export default App;
